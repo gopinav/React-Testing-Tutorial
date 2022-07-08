7a43de0ea05a3371aaf6a105a0db16b5463b0007
Vishwas <vishwas.g5@gmail.com>
2022-07-08 16:09:57 +0530

    Jest conventions

--- fundamentals-demo/src/__tests__/GreetTest.tsx
+++ fundamentals-demo/src/__tests__/GreetTest.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { Greet } from "./Greet";
+import { Greet } from "../components/greet/Greet";
 
 describe("Greet", () => {
   test("renders correctly", () => {
--- fundamentals-demo/src/components/greet/Greet.spec.tsx
+++ fundamentals-demo/src/components/greet/Greet.spec.tsx
@@ -2,13 +2,13 @@ import { render, screen } from "@testing-library/react";
 import { Greet } from "./Greet";
 
 describe("Greet", () => {
-  test("renders correctly", () => {
+  it("renders correctly", () => {
     render(<Greet />);
     const textElement = screen.getByText("Hello");
     expect(textElement).toBeInTheDocument();
   });
 
-  test("renders a name", () => {
+  it("renders a name", () => {
     render(<Greet name="Vishwas" />);
     const textElement = screen.getByText("Hello Vishwas");
     expect(textElement).toBeInTheDocument();
