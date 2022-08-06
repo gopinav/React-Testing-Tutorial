a95b953da1b44ddfe59f41f524451ce2f562933a
Vishwas <vishwas.g5@gmail.com>
2022-08-06 08:44:52 +0530

    Revert changes

--- fundamentals-demo/src/components/application/Application.test.tsx
+++ fundamentals-demo/src/components/application/Application.test.tsx
@@ -35,7 +35,7 @@ describe('Application', () => {
 
     const submitElement = screen.getByRole('button')
     expect(submitElement).toBeInTheDocument()
-    expect(submitElement).not.toBeEnabled()
+    expect(submitElement).toBeDisabled()
 
     /** getByLabelText */
 
